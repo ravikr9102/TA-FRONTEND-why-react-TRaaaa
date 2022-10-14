1873ec90a93af34fd2b57d5b225642d32d861b26
Ravi kumar <rkravikr9102@gmail.com>
2022-10-14 21:22:20 +0530

    Done Assignment HTML to React

---
+++ block-BRaaav/hydro/src/components/App.js
@@ -0,0 +1,23 @@
+import Header from "./header";
+import Hero from "./hero";
+import About from "./about";
+import Blog from "./blog";
+import Work from "./work";
+import Contact from "./contact";
+import Footer from "./footer";
+
+function App(props){
+    return (
+        <>
+            <Header />
+            <Hero />
+            <About />
+            <Blog />
+            <Work />
+            <Contact />
+            <Footer />
+        </>
+    )
+}
+
+export default App;
